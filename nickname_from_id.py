9d405003860154b21c7e6bb2521d19810a9be1e8
m1amgn <59062076+m1amgn@users.noreply.github.com>
2024-08-26 20:02:00 +0300

    some fixes

--- nickname_from_id.py
+++ nickname_from_id.py
@@ -5,21 +5,17 @@ from tortoise import Tortoise, run_async
 from models import Influencer
 from dotenv import load_dotenv
 
-# Загрузка переменных окружения
 load_dotenv()
 
-# Инициализация бота
 bot = Bot(token=os.getenv('TG_BOT_TOKEN'))
 
-# Настройка соединения с базой данных
 async def init():
     await Tortoise.init(
-        db_url='postgres://myuser:12345@localhost/raptor',
+        db_url=os.getenv('DB_URL'),
         modules={'models': ['models']}
     )
     await Tortoise.generate_schemas()
 
-# Функция для получения ников и сохранения их в JSON
 async def fetch_usernames():
     await init()
     influencers = await Influencer.all()
@@ -28,19 +24,16 @@ async def fetch_usernames():
     for influencer in influencers:
         user_id = influencer.telegram_id
         try:
-            # Получаем информацию о пользователе по его ID
             user = await bot.get_chat(user_id)
             id_nickname_map[user_id] = user.username if user.username else "No username"
         except Exception as e:
             print(f"Failed to fetch username for ID {user_id}: {e}")
             id_nickname_map[user_id] = "Failed to fetch"
 
-    # Сохранение в JSON
     with open('influencers_usernames.json', 'w') as f:
         json.dump(id_nickname_map, f, ensure_ascii=False, indent=4)
 
     await Tortoise.close_connections()
 
-# Запуск скрипта
 if __name__ == "__main__":
     run_async(fetch_usernames())
